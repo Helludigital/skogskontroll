0f8e3410edd9c5faa46239887815d6b86738b3de
CARJOYConnect <simon@carjoy.se>
2025-04-09 10:56:41 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -79,7 +79,7 @@ const Footer = () => {
                   <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                   <polyline points="22,6 12,13 2,6"></polyline>
                 </svg>
-                <span className="text-forest-300">skogskontroll@icloud.com</span>
+                <span className="text-forest-300">info@skogskontroll.se</span>
               </li>
             </ul>
           </div>
